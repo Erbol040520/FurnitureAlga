706168ac24b32b14dd49b92aed0c90e85f3b45f1
Erbol040520 <65028887+Erbol040520@users.noreply.github.com>
2020-10-15 03:15:55 +0500

    Add files via upload

---
+++ MainBlog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import FurnitureCard
+from .models import FurnitureCardTwo
+
+admin.site.register(FurnitureCard)
+admin.site.register(FurnitureCardTwo)
